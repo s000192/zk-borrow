8007ba21d49a68c4b0505a76f8be6898ec9480bc
Calvin Tang <tangkiwan@gmail.com>
2022-07-03 15:35:57 +0800

    chore: add warning note on main page

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,19 +1,25 @@
 import { Grid } from '@mui/material';
 import type { NextPage } from 'next'
 import DashboardTable from '../components/markets/DashboardTable';
+import InfoBox from '../components/shared/InfoBox';
 
 import "../i18n";
 
 const Home: NextPage = () => {
   return (
-    <Grid container direction="row" alignItems="stretch">
-      <Grid item xs>
-        <DashboardTable side={"supply"} />
+    <>
+      <Grid container direction="row" alignItems="stretch">
+        <Grid item xs>
+          <DashboardTable side={"supply"} />
+        </Grid>
+        <Grid item xs>
+          <DashboardTable side={"borrow"} />
+        </Grid>
       </Grid>
-      <Grid item xs>
-        <DashboardTable side={"borrow"} />
-      </Grid>
-    </Grid>
+
+      <InfoBox>Please mint test tokens to use ZkJoe.</InfoBox>
+      <InfoBox>ZkJoe is currently using a mock oracle.</InfoBox>
+    </>
   )
 }
 
